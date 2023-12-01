df401063bb1473b3fe3fac636c764dc01c972ff3
hungnt <hungnt@etc.vn>
2023-12-01 08:31:17 +0700

    perf: update setting_signout_tile

--- lib/features/settings/screens/settings/widgets/setting_signout_tile.dart
+++ lib/features/settings/screens/settings/widgets/setting_signout_tile.dart
@@ -1,6 +1,5 @@
 import 'package:erb_ui/erb_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:erb_shared/extensions.dart';
 
 import 'package:erb_flutter_boilerplate/core/presentation/hook/hook.dart';
 import 'package:erb_flutter_boilerplate/core/presentation/utils/riverpod_framework.dart';
@@ -16,12 +15,18 @@ class SettingSignOutTile extends ERbSettingsTile {
     return HookConsumer(builder: (context, ref, child) {
       final t = useI18n();
 
+      final isSigned = ref.watch(isSignedProvider);
+      final isSignOutProgress = ref.watch(signOutControllerProvider).isLoading;
+
+      final enabled = useMemoized(() {
+        if (!isSigned || isSignOutProgress) return false;
+        return true;
+      }, [isSigned, isSignOutProgress]);
+
       return ERbSettingsTile(
         leading: const Icon(Icons.notification_important_rounded),
         title: Text(t.featureSettings.account.signout),
-        enabled: (!ref.watch(signOutControllerProvider).isLoading)
-                .takeIf((it) => ref.watch(isSignedProvider)) ??
-            false,
+        enabled: enabled,
         onPressed: (context) {
           ref.read(signOutControllerProvider.notifier).signOut();
         },
