86b31e5a21b03bb17d5bc0b5d440e4c1b2d0dace
ETC\anhlp <anhlp@etc.vn>
2023-12-14 15:19:09 +0700

    docs: note generate env issues

--- packages/env/lib/src/env/env_flavor.dart
+++ packages/env/lib/src/env/env_flavor.dart
@@ -9,6 +9,16 @@ import 'impl/env_staging.dart';
 abstract class EnvFlavor implements EnvFields {
   static EnvFlavor? _instance;
 
+  // NOTE: When modifying the .env file, the generator might not pick up the
+  // change due to dart-lang/build#967. If that happens simply clean the build
+  // cache and run the generator again.
+  //
+  // ```
+  // dart run build_runner clean
+  // dart run build_runner build --delete-conflicting-outputs
+  // ```
+  //
+  // https://github.com/petercinibulk/envied/issues/6
   factory EnvFlavor() {
     return _instance ??= switch (AppFlavor.fromEnvironment) {
       Flavor.prod || Flavor.beta => const EnvProd(),
